396c97cb6678fc30c46293bc96a8cf68a1487aed
t3yamoto <3yamoto.dev@gmail.com>
2023-09-06 09:14:13 +0900

    migrate to flask

--- app/main.py
+++ app/main.py
@@ -1,14 +1,7 @@
-import numpy as np
-import altair as alt
-import pandas as pd
-import streamlit as st
+from flask import Flask
 
-st.header('Streamlit on App runner')
+app = Flask(__name__)
 
-df = pd.DataFrame(
-     np.random.randn(200, 3),
-     columns=['a', 'b', 'c'])
-c = alt.Chart(df).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-st.write(c)
-
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
--- infra/infra/infra_stack.py
+++ infra/infra/infra_stack.py
@@ -1,7 +1,6 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_apprunner as apprunner,
 )
 from constructs import Construct
 
@@ -10,10 +9,35 @@ class InfraStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create a source code repository for App Runner
+        source_repository = apprunner.CfnService.SourceConfigurationProperty(
+            authentication_configuration=apprunner.CfnService.AuthenticationConfigurationProperty(
+                connection_arn='arn:aws:apprunner:ap-northeast-1:450677052036:connection/t3yamoto/b483998d25ad43c581a11edce5b80fb5', # Replace with your connection arn
+            ),
+            code_repository=apprunner.CfnService.CodeRepositoryProperty(
+                repository_url='https://github.com/t3yamoto/flask-on-app-runner',
+                source_code_version=apprunner.CfnService.SourceCodeVersionProperty(
+                    type='BRANCH',
+                    value='main'
+                ),
+                code_configuration=apprunner.CfnService.CodeConfigurationProperty(
+                    configuration_source='API',
+                    code_configuration_values=apprunner.CfnService.CodeConfigurationValuesProperty(
+                        runtime='PYTHON_3',
+                        build_command='cd app && pip install -r requirements.txt',
+                        start_command='cd app && flask --app main run --port 8080',
+                    ),
+                ),
+            ),
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "InfraQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create an App Runner service
+        app_runner_service = apprunner.CfnService(
+            self, 'AppRunnerService',
+            service_name='AppRunnerExampleService',
+            source_configuration=source_repository,
+            instance_configuration=apprunner.CfnService.InstanceConfigurationProperty(
+                cpu='1024',
+                memory='2048',
+            ),
+        )
